b9790b1ac7d0a998138e87d84ba4aa8182b6198e
Bernadette Werneke <bernadettewerneke@gmail.com>
2023-11-16 18:33:25 +0100

    Kommentare eingefügt

--- src/main/kotlin/Zauberin.kt
+++ src/main/kotlin/Zauberin.kt
@@ -1,10 +1,10 @@
-class Zauberin (name:String, heilPK:Int):Helden(name, heilPK,) {
+class Zauberin (name:String, heilPK:Int):Helden(name, heilPK) {
 
     //Zauber-Aktionen von Maleficient:
     //Petrificus Totalus (Versteinerungszauber)
     fun petrificusTotalus(gegner: Gegner) {
         this.schadenPK = (250..350).random()
-        var zauberName = "Petrificus Totalus (Versteinerungszauber)"
+        val zauberName = "Petrificus Totalus (Versteinerungszauber)"
         println("Zauberin ${this.name} (${this.heilPK} PK) zaubert mit $zauberName ($schadenPK SchadenPK).")
         println("Gegner: ${gegner.name} (${gegner.heilPK} PK)")
         gegner.heilPK -= schadenPK
@@ -20,7 +20,7 @@ class Zauberin (name:String, heilPK:Int):Helden(name, heilPK,) {
     //Avis (Vogel-Heraufbeschwörungszauber)
     fun avis(gegner: Gegner){
         this.schadenPK=(280..320).random()
-        var zauberName = "Avis (Vogel-Heraufbeschwörungszauber)"
+        val zauberName = "Avis (Vogel-Heraufbeschwörungszauber)"
         println("Zauberin ${this.name} (${this.heilPK} PK) zaubert mit $zauberName ($schadenPK SchadenPK).")
         println("Gegner: ${gegner.name} (${gegner.heilPK} PK)")
         gegner.heilPK -= schadenPK
@@ -36,7 +36,7 @@ class Zauberin (name:String, heilPK:Int):Helden(name, heilPK,) {
     //Expelliarmus (Entwaffnungszauber)
     fun expelliarmus(gegner: Gegner){
         schadenPK=(80..150).random()
-        var zauberName = "Expelliarmus (Entwaffnungszauber)"
+        val zauberName = "Expelliarmus (Entwaffnungszauber)"
         println("Zauberin ${this.name} (${this.heilPK} PK) zaubert mit $zauberName ($schadenPK SchadenPK).")
         println("Gegner: ${gegner.name} (${gegner.heilPK} PK)")
         gegner.heilPK -= schadenPK
@@ -52,7 +52,7 @@ class Zauberin (name:String, heilPK:Int):Helden(name, heilPK,) {
     //Incendio (Feuerzauber)
     fun incendio(gegner: Gegner){
         this.schadenPK=(150..250).random()
-        var zauberName = "Incendio (Feuerzauber)"
+        val zauberName = "Incendio (Feuerzauber)"
         println("Zauberin ${this.name} (${this.heilPK} PK) zaubert mit $zauberName ($schadenPK SchadenPK).")
         println("Gegner: ${gegner.name} (${gegner.heilPK} PK)")
         gegner.heilPK -= schadenPK
